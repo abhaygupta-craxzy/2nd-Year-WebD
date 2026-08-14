6c68044d77139fd66013e06be7c54e6a5d7a1afc
abhaygupta-craxzy <abhayloveporwal@gmail.com>
2026-08-14 11:57:43 +0530

    Day-3

--- day3/object.js
+++ day3/object.js
@@ -15,4 +15,22 @@ console.log(student.age);
 
 console.log("====== Array Of Object ======");
 const info = [{id:101,name:"Abhay",age:20},{id:102,name:"Aditya",age:20}];
-console.log(info);;
+console.log(info);
+
+
+
+console.log("====== Object Method ======");
+
+const emp = {
+    id: 102,
+    name: "Abhay",
+    age: 20,
+    display: function(){
+        console.log("ID:",this.id);
+        console.log("Name:",this.name);
+        console.log("Age:",this.age);
+    }
+};
+emp.display();
+
+
